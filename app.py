673d8c07874cf66c0a759b411d5a221272963d97
Igor Cabral Rocha Araujo <igor.rocha@poder360.com.br>
2026-01-02 14:39:47 -0300

    Teste: Alterando mensagem da home

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     # Pega a variável de ambiente que definimos no Terraform
     ambiente = os.environ.get("AMBIENTE", "Desenvolvimento")
-    return f"<h1>Ola! App rodando no ambiente: {ambiente}</h1>"
+    return f"<h1>Ola! App rodando no ambiente novo com auto: {ambiente}</h1>"
 
 if __name__ == "__main__":
     # O Cloud Run espera que a app rode na porta definida pela var PORT (padrao 8080)
